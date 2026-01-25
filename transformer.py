2fee016add4bcca50c64e8a16f8f1b0ba18925fa
aravinthen <arre0raj@gmail.com>
2026-01-25 23:11:47 +0000

    basic transformer architecture layout
    The very basic structure of the transformer. I wasn't able to add
    too much here due to running out of time, but I'll populate this
    over the next week when I have time.

---
+++ transformer.py
@@ -0,0 +1,32 @@
+# Program:     transformer.py
+# Author:      Aravinthen Rajkumar
+# Description: A reimplementation of the transformer from Vaswani et al. 2017
+
+import torch
+import torch.nn
+import math
+
+
+class Encoder(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+class Decoder(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+class Encoding(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+class Transformer(nn.Module):
+    """
+    This is a transformer written from scratch, intended to be as close to the 
+    model demonstrated in Attention is All You Need.
+    """
+    def __init__(self, ):
+        super().__init__()
